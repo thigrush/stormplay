1f7963f7a78d4c558e6c09831d7bd8a9e2829bb8
thigrush <thigrush@gmail.com>
2020-11-23 15:34:02 -0300

    style: finishes card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,10 @@
 const Card = () => (
-  <div className="card">
+  <div
+    className="card"
+    style={{
+      backgroundImage: `url(https://i.ytimg.com/vi/R7klyFU_6xM/mqdefault.jpg)`
+    }}
+  >
     <a href="#" className="card__link">
       <h3 className="card__title">VIDEO</h3>
       <p className="card__description">DESCRIPTION</p>
