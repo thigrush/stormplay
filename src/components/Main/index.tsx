57f727512a2fd8c8ba9ea31d569599e2e35bdef8
thigrush <thigrush@gmail.com>
2020-11-23 05:31:43 -0300

    feat: create main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,18 @@
 const Main = () => (
-  <main>
-    <h1>Storm Play</h1>
+  <main className="main">
+    <h2>Storm Play</h2>
+    <div className="grid">
+      <a href="#" className="card">
+        <h3>VIDEO</h3>
+        <p>DESCRIPTION</p>
+      </a>
+    </div>
+    <div className="grid">
+      <a href="#" className="card">
+        <h3>VIDEO</h3>
+        <p>DESCRIPTION</p>
+      </a>
+    </div>
   </main>
 )
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
-  it('should render the heading', () => {
+  it('should render the main', () => {
     render(<Main />)
 
     expect(
