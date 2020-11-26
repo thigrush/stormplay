07b845f7c9bde339282373ee4ba5d3093caf14af
thigrush <thigrush@gmail.com>
2020-11-26 19:20:58 -0300

    test: ajust errors in log and props

--- src/components/Card/test.tsx
+++ src/components/Card/test.tsx
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react'
 import { fetchVideo } from '../../services/api.service'
-
 import { Card } from '../'
 
 describe('<Card />', () => {
-  const videos = fetchVideo(20).then((data) => data.items)
-
   it('should render the card', async () => {
-    render(<Card {...await videos} />)
+    render(<Card card={await fetchVideo(20).then((data) => data.items[0])} />)
 
-    expect(screen.getAllByRole('heading', { level: 4 }).length > 10)
+    expect(screen.getAllByRole('heading', { level: 4 }).length === 15)
+
+    expect(
+      screen.getByRole('heading', { name: /Biggest/i })
+    ).toBeInTheDocument()
   })
 })
--- src/components/Caroussel/test.tsx
+++ src/components/Caroussel/test.tsx
@@ -1,13 +1,15 @@
-import { render, screen } from '@testing-library/react'
+import { cleanup, render, screen } from '@testing-library/react'
 
 import { Caroussel } from '../'
 
 describe('<Caroussel />', () => {
-  it('should render the caroussel', () => {
-    render(<Caroussel {...{ name: 'Esportes', id: 17 }} />)
+  it('should render the caroussel', async () => {
+    render(<Caroussel category={{ name: 'Esportes', id: 17 }} />)
 
     expect(
-      screen.getByRole('heading', { name: /Esportes/i })
+      await screen.getByRole('heading', { name: /Esportes/i })
     ).toBeInTheDocument()
+
+    cleanup()
   })
 })
