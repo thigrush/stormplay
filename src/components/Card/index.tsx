5d36335e5a277d5f2b78e13025afd82cbbbbf2a3
thigrush <thigrush@gmail.com>
2020-11-26 20:36:32 -0300

    feat: add filter in thumbnails and ajust size

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,13 +10,13 @@ export default function Card({ card }: { card: Cards }) {
     >
       <a href="#" className="card__link">
         <h4 className="card__title">
-          {card.snippet?.title.length > 20
-            ? card.snippet?.title.slice(0, 20) + '...'
+          {card.snippet?.title.length > 22
+            ? card.snippet?.title.slice(0, 22) + '...'
             : card.snippet?.title}
         </h4>
         <p className="card__description">
-          {card.snippet?.description.length > 25
-            ? card.snippet?.description.slice(0, 25) + '...'
+          {card.snippet?.description.length > 35
+            ? card.snippet?.description.slice(0, 35) + '...'
             : card.snippet?.description}
         </p>
       </a>
