2307d20293c38e07fd0c12b2a60eb390d379d711
thigrush <thigrush@gmail.com>
2020-11-26 23:04:28 -0300

    feat: add nextjs link component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,25 +1,28 @@
 import { Cards } from '../../types/card.type'
+import Link from 'next/link'
 
 export default function Card({ card }: { card: Cards }) {
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage: `url(${card.snippet?.thumbnails?.high?.url})`
-      }}
-    >
-      <a href="#" className="card__link">
-        <h4 className="card__title">
-          {card.snippet?.title.length > 22
-            ? card.snippet?.title.slice(0, 22) + '...'
-            : card.snippet?.title}
-        </h4>
-        <p className="card__description">
-          {card.snippet?.description.length > 35
-            ? card.snippet?.description.slice(0, 35) + '...'
-            : card.snippet?.description}
-        </p>
-      </a>
-    </div>
+    <Link href="/abc">
+      <div
+        className="card"
+        style={{
+          backgroundImage: `url(${card.snippet?.thumbnails?.high?.url})`
+        }}
+      >
+        <div className="card__content">
+          <h4 className="card__title">
+            {card.snippet?.title.length > 22
+              ? card.snippet?.title.slice(0, 22) + '...'
+              : card.snippet?.title}
+          </h4>
+          <p className="card__description">
+            {card.snippet?.description.length > 35
+              ? card.snippet?.description.slice(0, 35) + '...'
+              : card.snippet?.description}
+          </p>
+        </div>
+      </div>
+    </Link>
   )
 }
