ae455fb4e26114a0f2451cb074a25a95c0bc4fd4
thigrush <thigrush@gmail.com>
2020-11-27 00:04:02 -0300

    feat: create video component and ajust type param

--- src/components/Caroussel/test.tsx
+++ src/components/Caroussel/test.tsx
@@ -4,7 +4,7 @@ import { Caroussel } from '../'
 
 describe('<Caroussel />', () => {
   it('should render the caroussel', async () => {
-    render(<Caroussel category={{ name: 'Esportes', id: 17 }} />)
+    render(<Caroussel category={{ name: 'Esportes', id: '17' }} />)
 
     expect(
       await screen.getByRole('heading', { name: /Esportes/i })
---
+++ src/components/Video/index.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { fetchVideo } from '../../services/api.service'
+
+export default function Video({ id }: { id: string }) {
+  const [infoVideo, setVideo] = useState()
+
+  useEffect(() => {
+    fetchVideo(id, 'video').then((data) => setVideo(data))
+  }, [id])
+
+  console.log(infoVideo)
+
+  return (
+    <main className="main">
+      <h2>{id}</h2>
+    </main>
+  )
+}
