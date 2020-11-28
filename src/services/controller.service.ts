c0ac00fa509bfce14f069579e8dff118c2261e7f
thigrush <thigrush@gmail.com>
2020-11-28 12:03:46 -0300

    feat: create up function and reduce code

--- src/services/controller.service.ts
+++ src/services/controller.service.ts
@@ -10,37 +10,28 @@ export const startControl = (query: string) => {
   )
 }
 
-const moveRight = () => {
+const moveHorizontal = (direction: string) => {
   const selected = document.querySelector('.selected')
   if (selected) {
     const items = document.querySelectorAll(`.${selected.classList[0]}`)
     let index = Array.from(items).indexOf(selected)
-    if (items.length - 2 >= index) {
-      items[++index].classList.add('selected')
-    } else {
-      items[0]?.classList.add('selected')
+
+    if (direction === 'right') {
+      items.length - 2 >= index
+        ? items[++index].classList.add('selected')
+        : items[0]?.classList.add('selected')
+    } else if (direction === 'left') {
+      index > 0
+        ? items[--index].classList.add('selected')
+        : items[items.length - 1]?.classList.add('selected')
     }
+
     selected.classList.remove('selected')
   }
 }
 
-const moveLeft = () => {
+const moveVertical = (direction: string) => {
   const selected = document.querySelector('.selected')
-  if (selected) {
-    const items = document.querySelectorAll(`.${selected.classList[0]}`)
-    let index = Array.from(items).indexOf(selected)
-    if (index > 0) {
-      items[--index].classList.add('selected')
-    } else {
-      items[items.length - 1]?.classList.add('selected')
-    }
-    selected.classList.remove('selected')
-  }
-}
-
-const moveDown = () => {
-  const selected = document.querySelector('.selected')
-
   if (selected) {
     const parentSelected = <HTMLScriptElement>selected.parentElement
     const items = parentSelected.querySelectorAll(`.${selected.classList[0]}`)
@@ -50,21 +41,36 @@ const moveDown = () => {
     const indexParent = Array.from(itemsParent).indexOf(parentSelected)
     const index = Array.from(items).indexOf(selected)
 
-    let itemParent = itemsParent[indexParent + 1]?.querySelectorAll(
-      `.${selected.classList[0]}`
-    )
-    if (indexParent + 1 < itemsParent.length) {
-      console.log('index', index)
-      itemParent[index]?.classList.add('selected')
+    let itemParent = itemsParent[
+      direction === 'down' ? indexParent + 1 : indexParent - 1
+    ]?.querySelectorAll(`.${selected.classList[0]}`)
+
+    if (
+      direction === 'down' ? indexParent + 1 < itemsParent.length : itemParent
+    ) {
+      if (itemParent[index]) {
+        itemParent[index].classList.add('selected')
+      } else {
+        itemParent[itemParent?.length - 1]?.classList.add('selected')
+      }
     } else {
-      itemParent = itemsParent[0]?.querySelectorAll(`.${selected.classList[0]}`)
-      itemParent[index]?.classList.add('selected')
+      if (direction === 'down') {
+        itemParent = itemsParent[0]?.querySelectorAll(
+          `.${selected.classList[0]}`
+        )
+        itemParent[index]?.classList.add('selected')
+      } else if (direction === 'up') {
+        itemParent = itemsParent[itemsParent?.length - 1]?.querySelectorAll(
+          `.${selected.classList[0]}`
+        )
+        itemParent[index]
+          ? itemParent[index]?.classList.add('selected')
+          : itemParent[0]?.classList.add('selected')
+      }
     }
     selected.classList.remove('selected')
   }
 }
-
-// const moveUp = () => {}
 
 // const enterSelected = () => {}
 
@@ -72,18 +78,23 @@ export const activeControl = (event: { key: string }) => {
   switch (event.key) {
     case 'ArrowRight':
     case 'Right':
-      moveRight()
+      moveHorizontal('right')
       break
     case 'ArrowLeft':
     case 'Left':
-      moveLeft()
+      moveHorizontal('left')
       break
     case 'ArrowDown':
     case 'Down':
-      moveDown()
+      moveVertical('down')
       break
     case 'ArrowUp':
-      console.log('up')
+    case 'Up':
+      moveVertical('up')
+      break
+    case 'Enter':
+      console.log('enter teste')
       break
   }
+  console.log(event.key)
 }
